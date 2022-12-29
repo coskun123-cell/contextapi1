311f6ca358891e6b5d411e49c4564f993ed18b6c
Coskun Cabuk <coskuncabuk@Coskuns-MacBook-Air.local>
2022-12-29 22:47:30 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import MovieList from "./MovieList";
+import { MovieProvider } from "./MovieContext";
+import AddMovie from "./AddMovie";
+
+function App() {
+  return (
+    <MovieProvider>
+      <div className="App">
+        <Nav />
+        <AddMovie />
+        <MovieList />
+      </div>
+    </MovieProvider>
+  );
+}
+
+export default App;
